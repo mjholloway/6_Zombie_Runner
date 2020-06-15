161f7a3c98ceb249dc6d47cb39cc2b6c6549d15f
hollowaymj <michaeljholloway@gmail.com>
2020-06-15 17:34:41 -0400

    Added ammo pickup prefab

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerHealth>() != null)
+        {
+            print("Here's some ammo!");
+            Destroy(gameObject);
+        }
+    }
+}
